5ce85ebcd402de255d44a7adb6b8a3dd3aee6d79
Moaz Ayman <ma5746@fayoum.edu.eg>
2024-07-26 10:02:58 +0300

    Remove Unused Styles

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -21,8 +21,6 @@ export const Hero = () => {
         alt="Hero image of me"
         className={styles.heroImg}
       />
-      <div className={styles.topBlur} />
-      <div className={styles.bottomBlur} />
     </section>
   );
 };
