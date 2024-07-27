f0ca50fd637c975fb97e5f40194523b61f06781a
Moaz Ayman <ma5746@fayoum.edu.eg>
2024-07-27 11:33:09 +0300

    Add animations and Update styling in Projects module and ProjectCard component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,39 +2,60 @@ import React from "react";
 
 import styles from "./About.module.css";
 import { getImageUrl } from "../../utils";
+import ScrollAnimation from "react-animate-on-scroll";
 
 export const About = () => {
   return (
     <section className={styles.container} id="about">
       <h2 className={styles.title}>About</h2>
       <div className={styles.content}>
-        <img
-          src={getImageUrl("about/about.png")}
-          alt="Me sitting with a laptop"
-          className={styles.aboutImage}
-        />
-        <ul className={styles.aboutItems}>
-          <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
-            <div className={styles.aboutItemText}>
-              <h3>Frontend Developer</h3>
-              <p>
-                I'm a frontend developer with experience in building responsive
-                and optimized sites
-              </p>
-            </div>
-          </li>
-          <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
-            <div className={styles.aboutItemText}>
-              <h3>Backend Developer</h3>
-              <p>
-                I have experience developing fast and optimised back-end systems
-                and APIs
-              </p>
-            </div>
-          </li>
-        </ul>
+        <ScrollAnimation
+          animateIn="fadeInLeft"
+          animateOut="fadeOut"
+          duration={1.3}
+          animateOnce={true}
+        >
+          <img
+            src={getImageUrl("about/about.png")}
+            alt="Me sitting with a laptop"
+            className={styles.aboutImage}
+          />
+        </ScrollAnimation>
+        <ScrollAnimation
+          animateIn="fadeInRight"
+          animateOut="fadeOut"
+          duration={1.3}
+          animateOnce={true}
+        >
+          <ul className={styles.aboutItems}>
+            <li className={styles.aboutItem}>
+              <img
+                src={getImageUrl("about/cursorIcon.png")}
+                alt="Cursor icon"
+              />
+              <div className={styles.aboutItemText}>
+                <h3>Frontend Developer</h3>
+                <p>
+                  I'm a frontend developer with experience in building
+                  responsive and optimized sites
+                </p>
+              </div>
+            </li>
+            <li className={styles.aboutItem}>
+              <img
+                src={getImageUrl("about/serverIcon.png")}
+                alt="Server icon"
+              />
+              <div className={styles.aboutItemText}>
+                <h3>Backend Developer</h3>
+                <p>
+                  I have experience developing fast and optimised back-end
+                  systems and APIs
+                </p>
+              </div>
+            </li>
+          </ul>
+        </ScrollAnimation>
       </div>
     </section>
   );
